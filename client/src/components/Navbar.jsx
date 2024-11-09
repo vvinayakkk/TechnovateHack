cd4055cdce5f8ee6bb42ed8b3069eb9438942f7a
ruchir <ruchirkhare12@gmail.com>
2024-11-10 01:01:53 +0530

    changes in layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
-import { Home, Folder, FileText, BarChart, NotebookIcon, Atom, Menu, X, User2 } from 'lucide-react';
+import { Home, Folder, FileText, BarChart, NotebookIcon, Atom, Menu, X, User2, Leaf } from 'lucide-react';
 import ThemeToggle from './ThemeToggle';
 import User from './User';
 
@@ -19,14 +19,9 @@ const Navbar = () => {
                 <Link to="/" className="flex space-x-2 items-center">
                     <div className="flex flex-col justify-center space-y-[-6px]">
                         <div className="flex">
-                            <Atom className="h-6 text-black dark:text-white mr-2" />
-                            <h1 className="text-xl font-semibold font-pop text-black dark:text-white mb-1 text-center">
-                                EcoSphere
-                            </h1>
+                            <Leaf className="w-6 h-6 text-green-500 mr-2 motion-scale-in-[0.5] motion-rotate-in-[-10deg] motion-blur-in-[10px] motion-delay-[0.75s]/rotate motion-delay-[0.75s]/blur " />
+                            <h1 className="text-xl md:text-2xl font-bold">CarbonTrace</h1>
                         </div>
-                        <p className="text-xs text-gray-400 dark:text-gray-500 tracking-tight font-pop text-center">
-                            Empowering Sustainability
-                        </p>
                     </div>
                 </Link>
 
@@ -78,8 +73,8 @@ const Navbar = () => {
             <nav className="md:hidden fixed top-0 left-0 w-full bg-white dark:bg-black shadow-lg z-50">
                 <div className="flex justify-between items-center p-4">
                     <Link to="/" className="flex items-center space-x-2">
-                        <Atom className="h-6 text-black dark:text-white" />
-                        <h1 className="text-xl font-semibold text-black dark:text-white">EcoSphere</h1>
+                        <Leaf className="w-6 h-6 text-green-500 mr-2 motion-scale-in-[0.5] motion-rotate-in-[-10deg] motion-blur-in-[10px] motion-delay-[0.75s]/rotate motion-delay-[0.75s]/blur " />
+                        <h1 className="text-xl md:text-2xl font-bold">CarbonTrace</h1>
                     </Link>
 
                     <div className="flex">
