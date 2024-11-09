fb01482059fde9685b1daed898c4e147481db1c8
ruchir <ruchirkhare12@gmail.com>
2024-11-09 18:05:28 +0530

    padding fixes

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -8,14 +8,14 @@ const MainLayout = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-black text-gray-800 dark:text-gray-200">
       <Navbar />
-      <header className="p-4 flex justify-between items-center">
+      <header className="pr-4 pt-3 flex justify-between items-center">
         <h1 className="text-2xl"></h1>
         <div className="flex justify-around">
           <ThemeToggle />
           <User />
         </div>
       </header>
-      <main className="p-4">
+      <main >
         <div className="ml-0 mr-0 md:ml-[15%] md:mr-[2%] lg:ml-[15%] lg:mr-[2%]">
           <Outlet />
         </div>
